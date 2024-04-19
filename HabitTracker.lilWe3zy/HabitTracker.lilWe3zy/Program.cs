9451abf98cec28bbf492b8d73b43c185169363f5
armin <arminnel@proton.me>
2024-04-19 20:05:51 +0200

    initial commit

---
+++ HabitTracker.lilWe3zy/HabitTracker.lilWe3zy/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker.lilWe3zy;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var connection = new SqliteConnection("Data Source=habitTracker.db"))
+        {
+            // Creates file if it does not exist
+            connection.Open();
+
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT *";
+        }
+    }
+}
